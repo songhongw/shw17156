f2d7c2178ce44bc338b1b4ce0e442534acba3f2f
songhongw <m13718232153@163.com>
2017-11-01 17:09:24 +0800

    change

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <div class="test">{{msg}}:{{age}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      age:22
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.test{background: red;}
+</style>
