e701a21e0986eeb4df9767edb27946ef86a3e078
songhongw <m13718232153@163.com>
2017-11-06 16:12:21 +0800

    change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,37 +1,56 @@
 <template>
 	<div>
- 		<home-header/>
- 		<swiper-content/>
- 		<home-category/>
- 		<listitem-content/>
- 		<list-hotcommend/>
- 		<weekList/>
+ 		<index-header/>
+ 		<index-swiper :swiperInfo="swiperInfo"/>
+ 		<index-category/>
+ 		<index-hotcommend/>
+ 		<index-iconText/>
+ 		<index-weekList/>
  	</div>
 </template>
 
 <script>
-
-import HeaderComponent from "./Header";
-import SwiperComponent from "./Swiper";
-
-import CategoryComponent from "./Category";
-
-import CommendComponent from "./hotcommend";
-import ListItemComponent from "./ListItem";
-import weekListComponent from "./weekList";
+import Header from "./components/Header";
+import Swiper from "./components/Swiper";
+import Category from "./components/Category";
+import HotCommend from "./components/hotcommend";
+import IconText from "./components/ListItem";
+import Weekend from "./components/weekList";
+import axios from 'axios';
 
 
 export default {
+	data(){
+		return{
+			swiperInfo:[]
+		}
+	},
 	components: {
-		"home-header": HeaderComponent,
-		"swiper-content": SwiperComponent,
-
-		"home-category":CategoryComponent,
-
-		"list-hotcommend": CommendComponent,
-		"listitem-content":ListItemComponent,
-		"weekList": weekListComponent
-
+		"index-header": Header,
+		"index-swiper": Swiper,
+		"index-category":Category,
+		"index-hotcommend": HotCommend,
+		"index-iconText":IconText,
+		"index-weekList": Weekend
+	},
+	methods:{
+		getDataSwiper(){
+			axios.get('/static/index.json')
+			.then( this.hanDleSwiperSucc.bind( this ) )
+			.catch( this.hanDleSwiperError( this ) );
+		},
+		hanDleSwiperSucc( response ){
+			if( response.status === 200 ){
+				var data = response.data.data;
+				console.log(response.data.data.swiperInfo);
+				this.swiperInfo = data.swiperInfo;
+			}
+		},
+		hanDleSwiperError( error ){
+		}
+	},
+	mounted(){
+		this.getDataSwiper();
 	}
 }
 </script>
